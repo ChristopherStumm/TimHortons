a4e84dd22e6d0449d5a3ce32314eb27a690a9d47
Mats77 <m.wieben@yahoo.de>
2015-07-17 15:04:58 +0200

    asd

--- src/activemq/ERPDataListener.java
+++ src/activemq/ERPDataListener.java
@@ -1,8 +1,5 @@
 package activemq;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -19,9 +16,7 @@ import model.ERPData;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 
 import utils.LogFileReader;
 
