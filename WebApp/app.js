c7cd3ea7a6e9294c845bb17b990e4b9ad27e4132
Lucas_Schlemm <Lucas@Schlemms.de>
2015-07-20 14:53:26 -0400

    d3.js wieder gelöscht....

--- WebApp/app.js
+++ WebApp/app.js
@@ -1,15 +1,11 @@
 'use strict';
 
-
-angular.module('d3', []);
-angular.module('timHortons.directives', ['d3']);
 angular.module('angularCharts', []);
 
 angular.module('TimHortons', [
  'ngRoute',
  'mobile-angular-ui',
  'timHortons.Dashboard',
- 'timHortons.directives',
  'timHortons.Ana1',
  'timHortons.Ana2',
  ])
