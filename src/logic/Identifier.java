aef749e27e1dccba691bd1e86f0aff3c0fa8a577
Vincent <vincentmzw@web.de>
2015-07-20 20:06:49 -0400

    More info about product station

--- src/logic/Identifier.java
+++ src/logic/Identifier.java
@@ -127,7 +127,9 @@ ArrayList<Product> productList = new ArrayList<>();
 	
 	private String findOutId(int stationOfEvent, boolean finished, OPCDataItem item){
 		int index = -1;
+		System.out.println("Station of Event: " + stationOfEvent);
 		for (int i=0; i < productList.size(); i++){
+			System.out.println("Station of Product: " + productList.get(i).getStation());
 			if (stationOfEvent != 6 && stationOfEvent != 10){
 			if (productList.get(i).getStation() == (stationOfEvent-1)){
 				index = i;
