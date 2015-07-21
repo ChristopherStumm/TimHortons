f1d1db5f0c2944d3a6227bcd81e1a909ae29826b
Mats77 <m.wieben@yahoo.de>
2015-07-21 16:18:59 +0200

    worked on further graphics in ana1/ana2

--- WebApp/ana2/ana2.js
+++ WebApp/ana2/ana2.js
@@ -1,15 +1,40 @@
 'use strict';
 
+google.load('visualization', '1', {
+    packages: ['corechart']
+});
+google.setOnLoadCallback(function () {
+    angular.bootstrap(document.body, ['myApp']);
+});
+
 angular.module('timHortons.Ana2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-	/* Routing zur Startseite mit Neuladen der Seite beim Aufrufen der Startseite */
-	$routeProvider.when('/ana2', {
-		templateUrl: 'ana2/ana2.html',
-		controller: 'Ana2Controller as ana2Ctrl',
-		reloadOnSearch: true});
+.config(['$routeProvider',
+    function ($routeProvider) {
+        /* Routing zur Startseite mit Neuladen der Seite beim Aufrufen der Startseite */
+        $routeProvider.when('/ana2', {
+            templateUrl: 'ana2/ana2.html',
+            controller: 'Ana2Controller as ana2Ctrl',
+            reloadOnSearch: true
+        });
 }])
 
-.controller('Ana2Controller', ['$scope', function($scope){
-	
+.controller('Ana2Controller', ['$scope',
+    function ($scope) {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004', 1000, 400],
+          ['2005', 1170, 460],
+          ['2006', 660, 1120],
+          ['2007', 1030, 540]
+        ]);
+
+        var options = {
+            title: 'Company Performance',
+            isStacked: true
+        };
+
+        var chart = new google.visualization.BarChart(document.getElementById('chartdiv'));
+        chart.draw(data, options);
+
 }])
