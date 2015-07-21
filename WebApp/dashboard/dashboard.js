7b0b4180a2b8f317a22d4f7fa92e59b576ba1d21
Mats77 <m.wieben@yahoo.de>
2015-07-21 05:38:46 +0200

    TOLLE Änderungen am Dashboard

--- WebApp/dashboard/dashboard.js
+++ WebApp/dashboard/dashboard.js
@@ -1,7 +1,6 @@
 'use strict';
 
-//var url = "http://personalchef.ddns.net:3001/test/"
-var url = "data.json"
+var url = "http://personalchef.ddns.net:3001/";
 var app = angular.module('timHortons.Dashboard', ['ngRoute', 'angularCharts'])
 
 .config(['$routeProvider',
@@ -16,169 +15,121 @@ var app = angular.module('timHortons.Dashboard', ['ngRoute', 'angularCharts'])
 
 .controller('DashboardController', ['$scope', '$http', '$timeout',
     function ($scope, $http, $timeout) {
-        $scope.title = "DemoCtrl";
-        $scope.d3Data = [
-            {
-                name: "Greg",
-                score: 98
-            },
-            {
-                name: "Ari",
-                score: 96
-            },
-            {
-                name: "Loser",
-                score: 48
-            }
-        ];
+        $scope.avMillSpeed = 20;
+        $scope.avDrillSpeed = 30;
+        $scope.avMillHeat = 40;
+        $scope.avDrillHeat = 50;
+        $scope.avRuntime = 60;
+        $scope.names = [{
+            "Name": "Alfreds Futterkiste",
+            "City": "Berlin",
+            "Country": "Germany"
+            }, {
+            "Name": "Ana Trujillo Emparedados y helados",
+            "City": "México D.F.",
+            "Country": "Mexico"
+            }, {
+            "Name": "Antonio Moreno Taquería",
+            "City": "México D.F.",
+            "Country": "Mexico"
+            }, {
+            "Name": "Around the Horn",
+            "City": "London",
+            "Country": "UK"
+            }, {
+            "Name": "B's Beverages",
+            "City": "London",
+            "Country": "UK"
+            }, {
+            "Name": "Berglunds snabbköp",
+            "City": "Luleå",
+            "Country": "Sweden"
+            }, {
+            "Name": "Blauer See Delikatessen",
+            "City": "Mannheim",
+            "Country": "Germany"
+            }, {
+            "Name": "Blondel père et fils",
+            "City": "Strasbourg",
+            "Country": "France"
+            }, {
+            "Name": "Bólido Comidas preparadas",
+            "City": "Madrid",
+            "Country": "Spain"
+            }, {
+            "Name": "Bon app'",
+            "City": "Marseille",
+            "Country": "France"
+            }, {
+            "Name": "Bottom-Dollar Marketse",
+            "City": "Tsawassen",
+            "Country": "Canada"
+            }, {
+            "Name": "Cactus Comidas para llevar",
+            "City": "Buenos Aires",
+            "Country": "Argentina"
+            }, {
+            "Name": "Centro comercial Moctezuma",
+            "City": "México D.F.",
+            "Country": "Mexico"
+            }, {
+            "Name": "Chop-suey Chinese",
+            "City": "Bern",
+            "Country": "Switzerland"
+            }, {
+            "Name": "Comércio Mineiro",
+            "City": "São Paulo",
+            "Country": "Brazil"
+            }]
+        console.log($scope.names)
 
-        $scope.salesData = [
-            {
-                hour: 1,
-                sales: 54
-            },
-            {
-                hour: 2,
-                sales: 66
-            },
-            {
-                hour: 3,
-                sales: 77
-            },
-            {
-                hour: 4,
-                sales: 70
-            },
-            {
-                hour: 5,
-                sales: 60
-            },
-            {
-                hour: 6,
-                sales: 63
-            },
-            {
-                hour: 7,
-                sales: 55
-            },
-            {
-                hour: 8,
-                sales: 47
-            },
-            {
-                hour: 9,
-                sales: 55
-            },
-            {
-                hour: 10,
-                sales: 30
-            }
-  ];
+        var time = "products?time=0.5&";
+        var status = "status=TOTAL&";
+        var sum = "sum=false";
+        var request = url + time + status + sum;
 
         (function tick() {
-            $http.get(url).success(function (data) {
-                $scope.data = data;
-                $scope.d3Data[0].score = data.value1
-                $scope.d3Data[1].score = data.value2
-                $scope.d3Data[2].score = data.value3
-                console.log(data)
-                $timeout(tick, 10000);
+            $http.get(request).success(function (data) {
+
+                //                for (var i = 0; i < data.length; i++) {
+                //                    for (var j = 0; j < data[i].data.length; j++) {
+                //                        if (data[i].data[j].itemName == "Lichtschranke 1") {
+                //                            console.log(data[i].data[j].status)
+                //                        }
+                //                    }
+                //                }
+                //                $scope.data = data;
+
+                console.log(data.length);
+                $timeout(tick, 100000000000);
             });
         })();
-}])
 
-app.directive("linearChart", function ($window, $parse) {
-    return {
-        restrict: "EA",
-        template: "<svg width='850' height='200'></svg>",
-        link: function (scope, elem, attrs) {
-            var exp = $parse(attrs.chartData);
-
-            var salesDataToPlot = exp(scope);
-            var padding = 20;
-            var pathClass = "path";
-            var xScale, yScale, xAxisGen, yAxisGen, lineFun;
-
-            var d3 = $window.d3;
-            var rawSvg = elem.find('svg');
-            var svg = d3.select(rawSvg[0]);
-
-            scope.$watchCollection(exp, function (newVal, oldVal) {
-                salesDataToPlot = newVal;
-                redrawLineChart();
+        request = url + "heatAndSpeed";
+        (function tick() {
+            $http.get(request).success(function (data) {
+                console.log(data);
+                $scope.avDrillHeat = data.drillingHeat.avg;
+                $scope.avDrillSpeed = data.drillingSpeed.avg;
+                $scope.avMillHeat = data.millingHeat.avg;
+                $scope.avMillSpeed = data.millingSpeed.avg;
+                $timeout(tick, 100000000000);
             });
+        })();
 
-            function setChartParameters() {
-
-                xScale = d3.scale.linear()
-                    .domain([salesDataToPlot[0].hour, salesDataToPlot[salesDataToPlot.length - 1].hour])
-                    .range([padding + 5, rawSvg.attr("width") - padding]);
-
-                yScale = d3.scale.linear()
-                    .domain([0, d3.max(salesDataToPlot, function (d) {
-                        return d.sales;
-                    })])
-                    .range([rawSvg.attr("height") - padding, 0]);
-
-                xAxisGen = d3.svg.axis()
-                    .scale(xScale)
-                    .orient("bottom")
-                    .ticks(salesDataToPlot.length - 1);
-
-                yAxisGen = d3.svg.axis()
-                    .scale(yScale)
-                    .orient("left")
-                    .ticks(5);
-
-                lineFun = d3.svg.line()
-                    .x(function (d) {
-                        return xScale(d.hour);
-                    })
-                    .y(function (d) {
-                        return yScale(d.sales);
-                    })
-                    .interpolate("basis");
-            }
-
-            function drawLineChart() {
-
-                setChartParameters();
-
-                svg.append("svg:g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0,180)")
-                    .call(xAxisGen);
-
-                svg.append("svg:g")
-                    .attr("class", "y axis")
-                    .attr("transform", "translate(20,0)")
-                    .call(yAxisGen);
-
-                svg.append("svg:path")
-                    .attr({
-                        d: lineFun(salesDataToPlot),
-                        "stroke": "blue",
-                        "stroke-width": 2,
-                        "fill": "none",
-                        "class": pathClass
-                    });
-            }
-
-            function redrawLineChart() {
-
-                setChartParameters();
-
-                svg.selectAll("g.y.axis").call(yAxisGen);
-
-                svg.selectAll("g.x.axis").call(xAxisGen);
-
-                svg.selectAll("." + pathClass)
-                    .attr({
-                        d: lineFun(salesDataToPlot)
-                    });
-            }
-
-            drawLineChart();
-        }
-    };
-});
+        $scope.speedLabels = ["12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"];
+        $scope.heatLabels = ["12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"];
+        $scope.speedSeries = ['Milling', 'Drilling'];
+        $scope.heatSeries = ['Milling', 'Drilling'];
+        $scope.heatData = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+        $scope.speedData = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+        $scope.onClick = function (points, evt) {
+            console.log(points, evt);
+        };
+}]);
