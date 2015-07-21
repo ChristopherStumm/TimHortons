a5ddd40da25a03cd8bc27f173f541527f5c51072
chrizzTs <c.mahlich@gmail.com>
2015-07-21 06:38:33 +0200

    DatabaseConnection update

--- src/connections/DatabaseConnection.java
+++ src/connections/DatabaseConnection.java
@@ -43,45 +43,5 @@ public class DatabaseConnection {
 		
 
 	}
-//	public static void main(String[] args) {
-//		DB db = DatabaseConnection.getConnection();
-//		DBCollection table = db.getCollection("bigdata");
-//		
-////		BasicDBObject query = new BasicDBObject();
-////		query.put("customerNumber", "mkyong");
-////		
-////		
-////		ERPData erp = new ERPData();
-////		erp.setCustomerNumber(23332);
-////		erp.setMaterialNumber(6566);
-////		erp.setOrderNumber("sdsdd5656kkkd");
-////		erp.setTimeStamp(new Date());
-////		
-////		Gson test = new Gson();
-////		String order = test.toJson(erp);
-////		
-////		BasicDBObject document = new BasicDBObject();
-////		document.put("customerNumber", 56656);
-////		document.put("order", order);
-////		table.insert(document);
-//		
-//	
-//		BasicDBObject searchQuery = new BasicDBObject().append("customerNumber", "77");
-//		
-//		BasicDBObject newDocument = new BasicDBObject();
-//		newDocument.append("$addToSet", new BasicDBObject().append("orders", 110));
-//
-//		
-//		table.update(searchQuery, newDocument);
-//		
-//		DBCursor cursor = table.find(searchQuery);
-//	 
-//		if(cursor.hasNext()==false) {
-//			System.out.println("dalse");
-//		}
-//		while (cursor.hasNext()) {
-//			System.out.println(cursor.next());
-//		}
-//	}
 
 }
